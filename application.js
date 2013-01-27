de9e6f176e5839e9a67cb6d1b39240b837418a50
egorbachurin <egorbachurin@gmail.com>
2013-01-28 01:20:49 +0700

    #-: link to current version

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 
 var idOpenTabWhatCd = null;
 var idOpenTabLastFm = null;
+var idOpenTabYoutube = null;
 
 function gotoWhatcd(info, tab) {
     var urlWhatCd = "https://what.cd/torrents.php?searchstr=" + info.selectionText;
@@ -26,12 +27,26 @@ function gotoLastfm(info, tab) {
     }
 }
 
+function gotoYoutube(info, tab) {
+    var urlYoutube = "http://www.youtube.com/results?high_definition=1&search_type=videos&uni=3&search_query=" + info.selectionText;
+
+    if(idOpenTabYoutube != null) {
+        chrome.tabs.update(idOpenTabYoutube, {url: urlYoutube});
+    } else {
+        chrome.tabs.create({url: urlYoutube}, function(tab){
+            idOpenTabYoutube = tab.id;
+        });
+    }
+}
+
 var id = chrome.contextMenus.create({"title": "Found %s in", "contexts":["selection", "link"]});
 
 chrome.contextMenus.create({"title": "Last.fm", "contexts":["selection", "link"],
     "onclick": gotoLastfm, "parentId": id});
 chrome.contextMenus.create({"title": "What.cd", "contexts":["selection", "link"],
     "onclick": gotoWhatcd, "parentId": id});
+chrome.contextMenus.create({"title": "Youtube.com", "contexts":["selection", "link"],
+    "onclick": gotoYoutube, "parentId": id});
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
     if(tabId == idOpenTabWhatCd) {
